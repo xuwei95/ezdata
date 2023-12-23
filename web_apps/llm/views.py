83b30c0f9b315ea2d6c2df22ee42aa0e55308324
xuwei95 <>
2023-12-23 17:50:22 +0800

    update readme

--- web_apps/llm/views.py
+++ web_apps/llm/views.py
@@ -2,12 +2,14 @@ from flask import Blueprint, request, jsonify
 from utils.common_utils import gen_json_response
 from utils.logger.logger import get_logger
 from utils.web_utils import get_req_para
+from utils.auth import validate_user, validate_permissions
 from web_apps.llm.services import data_chat
 logger = get_logger(p_name='system_log', f_name='llm', log_level='INFO')
 llm_bp = Blueprint('llm', __name__)
 
 
 @llm_bp.route('/data_chat', methods=['POST'])
+@validate_user
 def llm_data_chat():
     try:
         req_dict = get_req_para(request)
