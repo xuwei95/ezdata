835124d3b626ef5f8c6481ef460326b81b62062a
xuwei95 <>
2024-09-10 23:59:48 +0800

    网络连接支持递归抓取

--- web_apps/rag/extractor/http_url_extractor.py
+++ web_apps/rag/extractor/http_url_extractor.py
@@ -2,6 +2,8 @@ import os
 import requests
 from tempfile import NamedTemporaryFile
 from pathlib import Path
+import re
+from urllib.parse import urljoin, urlparse
 from web_apps.rag.extractor.extractor_base import BaseExtractor
 from langchain_core.documents import Document
 from web_apps.rag.extractor.csv_extractor import CSVExtractor
@@ -10,6 +12,13 @@ from web_apps.rag.extractor.html_extractor import HtmlExtractor
 from web_apps.rag.extractor.markdown_extractor import MarkdownExtractor
 from web_apps.rag.extractor.pdf_extractor import PdfExtractor
 from web_apps.rag.extractor.word_extractor import WordExtractor
+
+
+def extract_links_from_markdown(markdown_content):
+    # 使用正则表达式匹配Markdown格式的链接
+    link_pattern = re.compile(r'\[.*?\]\((.*?)\)')
+    links = link_pattern.findall(markdown_content)
+    return links
 
 
 class HttpUrlExtractor(BaseExtractor):
@@ -23,7 +32,9 @@ class HttpUrlExtractor(BaseExtractor):
             url,
             headers=None,
             ignore_links: bool = False,
-            ignore_images: bool = False
+            ignore_images: bool = False,
+            mode: str = 'scrape',
+            max_depth: int = 3
     ):
         """Initialize with url."""
         if headers is None:
@@ -34,13 +45,21 @@ class HttpUrlExtractor(BaseExtractor):
         self._headers = headers
         self.ignore_links = ignore_links
         self.ignore_images = ignore_images
-
+        self.mode = mode
+        self.max_depth = max_depth
+        self.visited_urls = set()  # 用于存储已经访问过的URL
 
     def extract(self) -> list[Document]:
-        url_suffix = Path(self._url).suffix
+        if self.mode == 'crawl':
+            return self._crawl(self._url, depth=0)
+        else:
+            return self._scrape(self._url)
+
+    def _scrape(self, url) -> list[Document]:
+        url_suffix = Path(url).suffix
 
         # Use requests to get the content and a NamedTemporaryFile to store it
-        with requests.get(self._url, headers=self._headers, stream=True) as response, \
+        with requests.get(url, headers=self._headers, stream=True) as response, \
                 NamedTemporaryFile(delete=False, suffix=url_suffix) as temp_file:
             try:
                 # Check if the request was successful
@@ -65,7 +84,8 @@ class HttpUrlExtractor(BaseExtractor):
                 elif file_extension == '.csv':
                     extractor = CSVExtractor(file_path, autodetect_encoding=True)
                 else:
-                    extractor = HtmlExtractor(file_path, ignore_links=self.ignore_links, ignore_images=self.ignore_images)
+                    extractor = HtmlExtractor(file_path, ignore_links=self.ignore_links,
+                                              ignore_images=self.ignore_images)
                 documents = extractor.extract()
                 # Close the file after writing to ensure all data is written
                 temp_file.close()
@@ -73,9 +93,30 @@ class HttpUrlExtractor(BaseExtractor):
                 os.unlink(file_path)
             return documents
 
+    def _crawl(self, url, depth) -> list[Document]:
+        if depth > self.max_depth:
+            return []
+
+        if url in self.visited_urls:
+            return []
+        self.visited_urls.add(url)
+
+        documents = self._scrape(url)
+
+        if isinstance(documents, list) and len(documents) > 0:
+            for doc in documents:
+                links = extract_links_from_markdown(doc.page_content)
+                links = [i for i in links if i != '' and i[0] == '/']
+                for link in links:
+                    absolute_url = urljoin(url, link)
+                    if urlparse(absolute_url).netloc == urlparse(url).netloc:
+                        documents.extend(self._crawl(absolute_url, depth + 1))
+
+        return documents
+
 
 if __name__ == '__main__':
-    url = 'https://akshare.akfamily.xyz/data/stock/stock.html'
-    extractor = HttpUrlExtractor(url)
+    url = 'http://ezdata.cloud'
+    extractor = HttpUrlExtractor(url, mode='crawl', max_depth=3)
     documents = extractor.extract()
     print(documents)
