42d1ff1647b76807af1fb6821af8601aa3fc9747
xuwei95 <>
2025-05-26 01:04:06 +0800

    rm tests

--- api/tests/akshare/demo1.py
+++
@@ -1,7 +0,0 @@
-import akshare as ak
-df = ak.fund_open_fund_info_em(fund="202108", indicator="单位净值走势")
-print(df)
-
-
-# stock_zh_a_spot_em_df = ak.stock_zh_a_spot_em()
-# print(stock_zh_a_spot_em_df)
--- api/tests/api_test.py
+++
@@ -1,6 +0,0 @@
-from web_apps.datamodel.services.datamodel_query_api_service import DataModelQueryApiService
-req_dict = {
-    "id": "e700bb6ddf514b7b86620f0634a35e94",
-    "operate": "status"
-}
-DataModelQueryApiService().operate_obj(req_dict)
